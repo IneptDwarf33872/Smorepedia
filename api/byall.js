05c9602a41feba1ab2b5089b60419298ae7cee32
IneptDwarf <IneptDwarf33872@gmail.com>
2024-04-27 17:00:29 -0400

    Fixed some stuff

--- api/byall.js
+++ api/byall.js
@@ -2,10 +2,12 @@
 const client = require("./client");
 
 export default async (req, res) => {
+    console.log("Found me!");
     if (req.method !== 'GET') {
       return res.status(405).json({ error: 'Method not allowed' });
     }
     try {
+        console.log("Trying...");
       const result = await client.query('SELECT * FROM pages');
       res.status(200).json(result.row);
     } catch (err) {
