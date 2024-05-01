86c5b55ed8874ec1ce856215c8ef5354dcb9b728
IneptDwarf <IneptDwarf33872@gmail.com>
2024-05-01 19:29:16 -0400

    change

--- api/client.js
+++ api/client.js
@@ -1,7 +1,7 @@
 const { Region } = require("oracle-nosqldb");
 const { ServiceType } = require("oracle-nosqldb/lib/constants");
 const NoSQLClient = require("oracle-nosqldb").NoSQLClient;
-
+const privateKeyPath = path.join(__dirname, '.oci', 'privateKey.pem');
 const client = new NoSQLClient({
   region: Region.US_ASHBURN_1,
   serviceType: ServiceType.CLOUD,
@@ -14,7 +14,7 @@ const client = new NoSQLClient({
 "ocid1.user.oc1..aaaaaaaanamjf2u5sdw57hsuuvi4adbeotpigzolekzehywix3ilz6osb3yq",
       fingerprint: "9e:23:38:8a:5b:b6:c7:b5:16:b6:72:1a:a5:92:9f:75",
       passphrase: "",
-      privateKeyFile: "./.oci/privateKey.pem",
+      privateKeyFile: privateKeyPath,
     },
   },
 });
