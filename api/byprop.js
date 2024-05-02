8367e6f57b83e8ce13216f428d6552c7ee41b848
IneptDwarf <IneptDwarf33872@gmail.com>
2024-05-02 19:36:59 -0400

    Changes

--- api/byprop.js
+++ api/byprop.js
@@ -1,7 +1,23 @@
 // api/byprop.js
 const client = require("./client");
 
+function applyCorsHeaders(res) {
+  // Allow all origins (be cautious with this in production)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Allow HTTP methods typically used in REST APIs
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+  // Allow common headers, add others if needed
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // If allowing credentials, ensure the origin isn't '*'
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+}
+
 export default async (req, res) => {
+  
+  applyCorsHeaders(res);
     if (req.method !== 'GET') {
       return res.status(405).json({ error: 'Method not allowed' });
     }
