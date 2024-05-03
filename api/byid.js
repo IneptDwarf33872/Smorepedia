13042817709f47c1a6dcdb7b059892fe45b765ac
IneptDwarf <IneptDwarf33872@gmail.com>
2024-05-03 18:37:59 -0400

    Changes

--- api/byid.js
+++ api/byid.js
@@ -21,11 +21,13 @@ export default async (req, res) => {
       return res.status(405).json({ error: 'Method not allowed' });
     }
   
-    const { prop } = req.query; // Note the use of query parameters for GET requests
-  
+    const { id } = req.query; // Note the use of query parameters for GET requests
+    const primaryKey = {
+      id: id,  // The ID you want to query by
+    };
     console.log(prop);
     try {
-      const result = await client.get('pages', { id: prop });
+      const result = await client.get('pages',primaryKey);
       res.status(200).json(result.rows);
     } catch (err) {
       res.status(500).json({ error: err });
