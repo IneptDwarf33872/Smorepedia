762cf04eeb1edc7a5f90675dfbf70b64f0eeafaa
IneptDwarf <IneptDwarf33872@gmail.com>
2024-05-03 18:52:39 -0400

    Changes

--- api/byid.js
+++ api/byid.js
@@ -27,7 +27,7 @@ export default async (req, res) => {
     };
     try {
       const result = await client.get('pages', primaryKey);
-      res.status(200).json(result);
+      res.status(200).json(result.row);
     } catch (err) {
       res.status(500).json({ error: err });
     }
