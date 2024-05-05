cb236ce2bc0554bdb4d1a7fe34b820ffd0f03b2e
IneptDwarf <IneptDwarf33872@gmail.com>
2024-05-04 20:30:57 -0400

    Added file Upload

--- api/upFile.js
+++ api/upFile.js
@@ -24,7 +24,7 @@ export default async (req, res) => {
     const { id, title, image, pageTitle, type } = req.body;
   
     try {
-        console.log("Image: " + JSON.stringify(req.body));
+        console.log("Image: " + req.body.image);
       const result = await client.put('Assets', req.body);
   
       res.status(200).json({ success: result.success });
