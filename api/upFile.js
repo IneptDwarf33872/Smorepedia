30e3ca263d5526d7fde3cbe5648fad8910c2b219
IneptDwarf <IneptDwarf33872@gmail.com>
2024-05-05 07:16:02 -0400

    Added file Upload

--- api/upFile.js
+++ api/upFile.js
@@ -1,5 +1,6 @@
 // api/add.js
 const client = require("./client");
+const CHUNK_SIZE = 1024 * 1024;
 function applyCorsHeaders(res) {
 
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -22,7 +23,7 @@ export default async (req, res) => {
     }
   
     const { id, title, image, pageTitle, type } = req.body;
-  
+    
     try {
         console.log("Image: " + image);
         if(typeof image == "string"){
@@ -30,18 +31,30 @@ export default async (req, res) => {
             const length = binaryString.length;
             const arrayBuffer = new ArrayBuffer(length); // Creates an ArrayBuffer of the correct size
             const uint8Array = new Uint8Array(arrayBuffer);
-          
+            const chunks = [];
+  for (let i = 0; i < uint8Array.length; i += chunkSize) {
+    const chunk = uint8Array.slice(i, i + chunkSize);
+    chunks.push(chunk);
+  }
             // Fill the Uint8Array with bytes from the decoded binary string
             for (let i = 0; i < length; i++) {
               uint8Array[i] = binaryString.charCodeAt(i);
             }
-            const result = await client.put('Assets', {
-                id,
-                title,
-                uint8Array,
-                pageTitle,
-                type,
-            });
+            for (let i = 0; i < chunks.length; i++) {
+                const chunk = chunks[i];
+              
+                const response = await client.put("Assets", {
+                  dataId,
+                  chunkIndex: i, // Index of this chunk
+                  totalChunks: chunks.length, // Total number of chunks
+                  data: chunk, // The chunk data
+                  title,
+                  pageTitle,
+                  type,
+                });
+              
+                console.log(`Sent chunk ${i + 1} of ${chunks.length}`);
+              }
             res.status(200).json({ success: result.success });
         }
   
