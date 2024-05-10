7531474ef3353b775531a0ebcafabe06ed64fa42
IneptDwarf <IneptDwarf33872@gmail.com>
2024-05-10 19:06:34 -0400

    Change:

--- api/upFile.js
+++ api/upFile.js
@@ -3,7 +3,7 @@ const busboy = require("busboy");
 
 function applyCorsHeaders(res) {
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 
